d3818c5ca437d52ea1031e4ae6ccb64ee5515cee
seethedata <seethedata@gmail.com>
2015-10-21 17:16:07 -0400

    Added support for multiple arrays in bin file

--- symmsummary.go
+++ symmsummary.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
@@ -34,28 +33,9 @@ type Disk struct {
 	Tech  string
 }
 
-func getMemory(exe string) <-chan string {
-	memPattern := regexp.MustCompile("[0-9]{5,6}$")
-	outchan := make(chan string)
-	go func() {
-		command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-memory"}}
-		output := command.Run()
-		for output.Scan() {
-			outputText := output.Text()
-			if memPattern.MatchString(outputText) {
-				outputText = symmtools.CleanMemorySize(outputText)
-			}
-			outchan <- outputText
-		}
-		close(outchan)
-	}()
-	return outchan
-}
-
 func getSymmList(exe string) <-chan string {
 	label := regexp.MustCompile("(DMX|VMAX)")
 	outchan := make(chan string)
-	arrays := make(map[string]Array)
 	go func() {
 		command := &symmtools.Worker{Cmd: exe, Args: []string{"list"}}
 		output := command.Run()
@@ -63,39 +43,57 @@ func getSymmList(exe string) <-chan string {
 			if label.MatchString(output.Text()) == true {
 				arrayData := strings.Fields(output.Text())
 				cache := arrayData[4]
-				devsInt, err := strconv.Atoi(arrayData[5])
-				check("List", err)
-				symdevsInt, err := strconv.Atoi(arrayData[6])
-				check("List", err)
-				arrays[arrayData[0]] = Array{
-					sid:     arrayData[0],
-					attach:  arrayData[1],
-					model:   arrayData[2],
-					mcode:   arrayData[3],
-					cache:   cache,
-					devs:    devsInt,
-					symdevs: symdevsInt}
-				break
+				//devsInt, err := strconv.Atoi(arrayData[5])
+				//check("List", err)
+				//symdevsInt, err := strconv.Atoi(arrayData[6])
+				//check("List", err)
+				sid := arrayData[0]
+				//attach:=  arrayData[1]
+				model := arrayData[2]
+				mcode := arrayData[3]
+				//devs:=    devsInt
+				//symdevs:= symdevsInt
+				outchan <- fmt.Sprintf("%s	Model: %s	Microcode: %s	Cache: %s\n", sid, model, mcode, symmtools.CleanMemorySize(cache))
 			}
-		}
-		for _, a := range arrays {
-			outchan <- fmt.Sprintf("Serial Number: %s	Model: %s	Microcode: %s	Cache: %s\n", a.sid, a.model, a.mcode, symmtools.CleanMemorySize(a.cache))
 		}
 		close(outchan)
 	}()
 	return outchan
 }
 
-func getCabinets(exe string) <-chan string {
+func getMemory(exe string, sids map[string]bool) <-chan string {
+	memPattern := regexp.MustCompile("[0-9]{5,6}$")
+	outchan := make(chan string)
+	go func() {
+		for sid := range sids {
+			command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-memory", "-sid", sid}}
+			output := command.Run()
+			for output.Scan() {
+				outputText := output.Text()
+				if memPattern.MatchString(outputText) {
+					outputText = symmtools.CleanMemorySize(outputText)
+				}
+				outchan <- outputText
+			}
+		}
+		close(outchan)
+	}()
+	return outchan
+}
+
+func getCabinets(exe string, sids map[string]bool) <-chan string {
 	cabinetLabel := regexp.MustCompile("Bay Location")
 	outchan := make(chan string)
 	go func() {
-		command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-bay_info"}}
-		output := command.Run()
-		for output.Scan() {
-			outputText := output.Text()
-			if cabinetLabel.MatchString(outputText) {
-				outchan <- outputText
+		for sid := range sids {
+		outchan<-"--------------------" + sid + "--------------------"
+			command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-bay_info", "-sid", sid}}
+			output := command.Run()
+			for output.Scan() {
+				outputText := output.Text()
+				if cabinetLabel.MatchString(outputText) {
+					outchan <- outputText
+				}
 			}
 		}
 		close(outchan)
@@ -103,16 +101,19 @@ func getCabinets(exe string) <-chan string {
 	return outchan
 }
 
-func getPools(exe string) <-chan string {
+func getPools(exe string, sids map[string]bool) <-chan string {
 	skipLabel := regexp.MustCompile("Symmetrix ID")
 	outchan := make(chan string)
 	go func() {
-		command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-thin", "-pool", "-gb"}}
-		output := command.Run()
-		for output.Scan() {
-			outputText := output.Text()
-			if !skipLabel.MatchString(outputText) {
-				outchan <- outputText
+		for sid := range sids {
+			outchan<-"--------------------" + sid + "--------------------"
+			command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-thin", "-pool", "-gb", "-sid", sid}}
+			output := command.Run()
+			for output.Scan() {
+				outputText := output.Text()
+				if !skipLabel.MatchString(outputText) {
+					outchan <- outputText
+				}
 			}
 		}
 		close(outchan)
@@ -120,7 +121,7 @@ func getPools(exe string) <-chan string {
 	return outchan
 }
 
-func getSoftware(exe string) <-chan string {
+func getSoftware(exe string, sids map[string]bool) <-chan string {
 	featureLabel := regexp.MustCompile("FeatureName")
 	capTypeLabel := regexp.MustCompile("FeatureCapacityType")
 	featureCapLabel := regexp.MustCompile("FeatureCapacity")
@@ -130,32 +131,33 @@ func getSoftware(exe string) <-chan string {
 	featureType := "X"
 	featureCap := "X"
 	go func() {
-		command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-features", "-enabled"}}
-		output := command.Run()
-		for output.Scan() {
-			outputText := whiteSpace.ReplaceAllString(output.Text(), "")
-			if featureLabel.MatchString(outputText) {
-				featureName = strings.Split(outputText, ":")[1]
-			} else if capTypeLabel.MatchString(outputText) {
-				featureType = strings.Split(outputText, ":")[1]
-			} else if featureCapLabel.MatchString(outputText) {
-				if featureType == "TBofTotalCapacity" {
-					featureCap = ": " + strings.Split(outputText, ":")[1]
-				} else {
-					featureCap = ""
+		for sid := range sids {
+			command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-features", "-enabled", "-sid", sid}}
+			output := command.Run()
+			for output.Scan() {
+				outputText := whiteSpace.ReplaceAllString(output.Text(), "")
+				if featureLabel.MatchString(outputText) {
+					featureName = strings.Split(outputText, ":")[1]
+				} else if capTypeLabel.MatchString(outputText) {
+					featureType = strings.Split(outputText, ":")[1]
+				} else if featureCapLabel.MatchString(outputText) {
+					if featureType == "TBofTotalCapacity" {
+						featureCap = ": " + strings.Split(outputText, ":")[1]
+					} else {
+						featureCap = ""
+					}
+				}
+				if featureName != "X" && featureType != "X" && featureCap != "X" {
+					outchan <- fmt.Sprintf("%s %s%s\n", featureName, featureType, featureCap)
+					featureName, featureType, featureCap = "X", "X", "X"
 				}
 			}
-			if featureName != "X" && featureType != "X" && featureCap != "X" {
-				outchan <- fmt.Sprintf("%s %s%s\n", featureName, featureType, featureCap)
-				featureName, featureType, featureCap = "X", "X", "X"
-			}
 		}
-
 		close(outchan)
 	}()
 	return outchan
 }
-func getDisks(exe string) <-chan string {
+func getDisks(exe string, sids map[string]bool) <-chan string {
 	targetLabel := regexp.MustCompile("(TargetID|Director)")
 	techLabel := regexp.MustCompile("Technology")
 	speedLabel := regexp.MustCompile("Speed")
@@ -169,82 +171,85 @@ func getDisks(exe string) <-chan string {
 	var speed string
 	var tech string
 	go func() {
-		command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-v"}}
-		output := command.Run()
-		tech = "X"
-		speed = "X"
-		size = "X"
-		for output.Scan() {
-			outputText := whiteSpace.ReplaceAllString(output.Text(), "")
-			if targetLabel.MatchString(outputText) {
-				tech = "X"
-				speed = "X"
-				size = "X"
-			} else if techLabel.MatchString(outputText) {
-				tech = strings.Split(outputText, ":")[1]
-			} else if speedLabel.MatchString(outputText) {
-				speed = symmtools.CleanSpeed(strings.Split(outputText, ":")[1])
-			} else if sizeLabel.MatchString(outputText) {
-				size = strings.Split(outputText, ":")[1]
-				if size == "0" {
+		for sid := range sids {
+			outchan<-"--------------------" + sid + "--------------------"
+			command := &symmtools.Worker{Cmd: exe, Args: []string{"list", "-v","-sid",sid}}
+			output := command.Run()
+			tech = "X"
+			speed = "X"
+			size = "X"
+			for output.Scan() {
+				outputText := whiteSpace.ReplaceAllString(output.Text(), "")
+				if targetLabel.MatchString(outputText) {
+					tech = "X"
+					speed = "X"
+					size = "X"
+				} else if techLabel.MatchString(outputText) {
+					tech = strings.Split(outputText, ":")[1]
+				} else if speedLabel.MatchString(outputText) {
+					speed = symmtools.CleanSpeed(strings.Split(outputText, ":")[1])
+				} else if sizeLabel.MatchString(outputText) {
+					size = strings.Split(outputText, ":")[1]
+					if size == "0" {
+						size = "X"
+						speed = "X"
+						tech = "X"
+					} else {
+						size = symmtools.CleanSize(size)
+					}
+				}
+
+				if tech != "X" && speed != "X" && size != "X" {
+					diskType := size + " " + speed + " " + tech + " "
+					disks[diskType]++
 					size = "X"
 					speed = "X"
 					tech = "X"
-				} else {
-					size = symmtools.CleanSize(size)
 				}
 			}
-
-			if tech != "X" && speed != "X" && size != "X" {
-				diskType := size + " " + speed + " " + tech + " "
-				disks[diskType] += 1
-				size = "X"
-				speed = "X"
-				tech = "X"
+			for d := range disks {
+				outchan <- fmt.Sprintf("(%d) %s\n", disks[d], d)
 			}
-		}
-		for d := range disks {
-			outchan <- fmt.Sprintf("(%d) %s\n", disks[d], d)
-		}
 
-		outchan <- "-----------HotSpares-----------"
-		command = &symmtools.Worker{Cmd: exe, Args: []string{"list", "-hotspares", "-v"}}
-		output = command.Run()
-		tech = "X"
-		speed = "X"
-		size = "X"
-		for output.Scan() {
-			outputText := whiteSpace.ReplaceAllString(output.Text(), "")
-			if targetLabel.MatchString(outputText) {
-				tech = "X"
-				speed = "X"
-				size = "X"
-			} else if techLabel.MatchString(outputText) {
-				tech = strings.Split(outputText, ":")[1]
-			} else if speedLabel.MatchString(outputText) {
-				speed = symmtools.CleanSpeed(strings.Split(outputText, ":")[1])
-			} else if hotspareSizeLabel.MatchString(outputText) {
-				size = strings.Split(outputText, ":")[1]
-				if size == "0" {
+			outchan <- "-----------HotSpares-----------"
+			command = &symmtools.Worker{Cmd: exe, Args: []string{"list", "-hotspares", "-v","-sid",sid}}
+			output = command.Run()
+			tech = "X"
+			speed = "X"
+			size = "X"
+			for output.Scan() {
+				outputText := whiteSpace.ReplaceAllString(output.Text(), "")
+				if targetLabel.MatchString(outputText) {
+					tech = "X"
+					speed = "X"
+					size = "X"
+				} else if techLabel.MatchString(outputText) {
+					tech = strings.Split(outputText, ":")[1]
+				} else if speedLabel.MatchString(outputText) {
+					speed = symmtools.CleanSpeed(strings.Split(outputText, ":")[1])
+				} else if hotspareSizeLabel.MatchString(outputText) {
+					size = strings.Split(outputText, ":")[1]
+					if size == "0" {
+						size = "X"
+						speed = "X"
+						tech = "X"
+					} else {
+						size = size
+					}
+				}
+
+				if tech != "X" && speed != "X" && size != "X" {
+					diskType := size + " " + speed + " " + tech + " "
+					hsdisks[diskType] += 1
 					size = "X"
 					speed = "X"
 					tech = "X"
-				} else {
-					size = size
 				}
 			}
 
-			if tech != "X" && speed != "X" && size != "X" {
-				diskType := size + " " + speed + " " + tech + " "
-				hsdisks[diskType] += 1
-				size = "X"
-				speed = "X"
-				tech = "X"
+			for d := range hsdisks {
+				outchan <- fmt.Sprintf("(%d) %s\n", hsdisks[d], d)
 			}
-		}
-
-		for d := range hsdisks {
-			outchan <- fmt.Sprintf("(%d) %s\n", hsdisks[d], d)
 		}
 		close(outchan)
 	}()
@@ -264,38 +269,37 @@ func main() {
 		log.Fatal(requiredVersion + " is required. Installed version is " + version)
 	}
 
-	symms := getSymmList(cfgexe)
-	memory := getMemory(cfgexe)
-	cabinets := getCabinets(cfgexe)
-	disks := getDisks(diskexe)
-	pools := getPools(cfgexe)
-	software := getSoftware(cfgexe)
+	//software := getSoftware(cfgexe)
+	sids := make(map[string]bool)
+
 	fmt.Println("-------------Symm--------------")
-	for output := range symms {
-		fmt.Printf("%s\n", output)
+	for symms := range getSymmList(cfgexe) {
+		sid := strings.Fields(symms)[0]
+		sids[sid] = true
+		fmt.Printf("%s\n", symms)
 	}
+
 	fmt.Println("-------------Memory--------------")
-	for output := range memory {
-		fmt.Printf("%s\n", output)
+	for memory := range getMemory(cfgexe, sids) {
+		fmt.Printf("%s\n", memory)
 	}
 
 	fmt.Println("-------------Cabinets--------------")
-	for output := range cabinets {
-		fmt.Printf("%s\n", output)
+	for cabinets := range getCabinets(cfgexe, sids) {
+		fmt.Printf("%s\n", cabinets)
 	}
 
 	fmt.Println("-------------Disks--------------")
-	for output := range disks {
-		fmt.Printf("%s\n", output)
+	for disks := range getDisks(diskexe, sids) {
+		fmt.Printf("%s\n", disks)
 	}
 
 	fmt.Println("-------------Pools--------------")
-	for output := range pools {
-		fmt.Printf("%s\n", output)
+	for pools := range getPools(cfgexe, sids) {
+		fmt.Printf("%s\n", pools)
 	}
-
-	fmt.Println("-------------Software (Experimental - May not be accurate)--------------")
+	/*fmt.Println("-------------Software (Experimental - May not be accurate)--------------")
 	for output := range software {
 		fmt.Printf("%s\n", output)
-	}
+	}*/
 }
